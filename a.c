058df3e3c473c3bdbd80b406b03b539f40ee09d2
Brian Johnson <bjohnson@rategenius.com>
2012-06-21 15:30:44 -0500

    wth

--- a.c
+++ a.c
@@ -1,7 +1,4 @@
-127.0.0.1	localhost localhost.localdomain JohnsonPrecision
-
-127.0.0.1 test.zend.com
-127.0.0.1 wwwb.rategenius.com
+aa127.0.0.1 wwwb.rategenius.com
 # The following lines are desirable for IPv6 capable hosts
 ::1     ip6-localhost ip6-loopback
 fe00::0 ip6-localnet
