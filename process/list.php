c79c1189cd1b0039f21b773926a2f5f72afcd28c
rbai <rbai@rategenius.com>
2014-02-28 21:51:32 -0600

    create statement

---
+++ process/list.php
@@ -0,0 +1,36 @@
+<?php
+require_once("objDBSQL.pclass");
+require_once("objLoadPage.pclass");
+require_once("functions.php");
+$versions=listAllVersion();
+{
+    foreach($versions as $key=>$v)
+    {
+        $link0="<a href='showPage.php?vid=".$key."&type=0' target='blank'> 0 </a>";
+        $link1="<a href='showPage.php?vid=".$key."&type=1' target='blank'> 1 </a>";
+        $link2="<a href='showPage.php?vid=".$key."&type=2' target='blank'> 2 </a>";
+        echo($v[0].'.....'.$v[1].$link0.'...'.$link1.'...'.$link2.'<br>');
+
+    }
+}
+function listAllVersion()
+{
+    $db=new objDBSQL();
+
+    $db->statement="
+    SELECT SITE_HOST_NAME,VERSION_DATE_TIME,VERSION_ID
+    FROM SITE
+    LEFT JOIN VERSION ON VERSION_SITE_ID=SITE_ID
+
+    ";
+    $db->execute();
+    $ret=array();
+    while($r=$db->next())
+    {
+        $ret[$r['VERSION_ID']][]=$r['SITE_HOST_NAME'];
+        $ret[$r['VERSION_ID']][]=$r['VERSION_DATE_TIME'];
+    }
+    return $ret;
+}
+
+
---
+++ process/variables.php
@@ -0,0 +1,9 @@
+<?php
+define('_PAGE_TYPE_RESOURCE',0);
+define('_LINK_TYPE_RESOURCE',1);
+define('_TEXT_TYPE_RESOURCE',2);
+define('_IMAGE_TYPE_RESOURCE',3);
+define('_CSS_TYPE_RESOURCE',4);
+define('_TXT_TYPE_RESOURCE',5);
+define('_VIDEO_TYPE_RESOURCE',6);
+
